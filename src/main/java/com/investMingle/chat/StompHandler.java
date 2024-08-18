3eb497e846016d29543310468716f34498d5ebd8
develpsh <bshyun1199@gmail.com>
2024-08-18 12:34:12 +0900

    2024.08.18 Interceptor 추가

---
+++ src/main/java/com/investMingle/chat/StompHandler.java
@@ -0,0 +1,44 @@
+package com.investMingle.chat;
+
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StompHandler implements ChannelInterceptor {
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);
+        System.out.println("full message:" + message);
+        System.out.println("auth:" + headerAccessor.getNativeHeader("Authorization"));
+        System.out.println(headerAccessor.getHeader("nativeHeaders").getClass());
+        if (StompCommand.CONNECT.equals(headerAccessor.getCommand())) {
+            System.out.println("msg: " + "conne");
+        }
+        //throw new MessagingException("no permission! ");
+        return message;
+    }
+
+    @Override
+    public void postSend(Message<?> message, MessageChannel channel, boolean sent) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        String sessionId = accessor.getSessionId();
+        System.out.println("sessionId = " + sessionId);
+        switch (accessor.getCommand()) {
+            case CONNECT:
+                // 유저가 Websocket으로 connect()를 한 뒤 호출됨
+                System.out.println("[postSend] connected");
+                break;
+            case DISCONNECT:
+                // 유저가 Websocket으로 disconnect() 를 한 뒤 호출됨 or 세션이 끊어졌을 때 발생함(페이지 이동~ 브라우저 닫기 등)
+                System.out.println("[postSend] disconnected");
+                break;
+            default:
+                break;
+        }
+    }
+}
--- src/main/java/com/investMingle/chat/config/webSocketConfiguration.java
+++ src/main/java/com/investMingle/chat/config/webSocketConfiguration.java
@@ -1,12 +1,18 @@
 package com.investMingle.chat.config;
 
+import com.investMingle.chat.StompHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class webSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
+    @Autowired
+    private StompHandler stompHandler;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -19,5 +25,10 @@ public class webSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/sub");
         registry.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
     }
 }
